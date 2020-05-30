6d95d48114b04049f675c266924b0255b3e8bfb7
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-30 00:43:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -20,3 +20,21 @@ end
 end 
 =end 
 
+Class School 
+  
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  
+def add_student(name, grade)
+  @grade = grade 
+  @name = name 
+  @roster[grade] = []
+  @roster[grade] << name
+end 
+
+end 
