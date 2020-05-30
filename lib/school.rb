12aeee9dcd472fda3b4bd6525c4556d4916e231c
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-30 00:03:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,8 +11,8 @@ class School
   def add_student(name, grade)
     @name = name 
     @grade = grade 
-    roster[grade] = []
-    roster[grade] << name 
+    @@roster[grade] = []
+    @@roster[grade] << name 
   end 
 
 end 
